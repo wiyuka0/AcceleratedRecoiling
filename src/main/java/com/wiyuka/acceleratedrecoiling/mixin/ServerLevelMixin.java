487bb5223e91cbdf5ca6c0bb75429c7d95f48233
CAT <Nekodays0@163.com>
2025-11-01 17:00:36 +0800

    change the mixin injection point

--- src/main/java/com/wiyuka/acceleratedrecoiling/mixin/ServerLevelMixin.java
+++ src/main/java/com/wiyuka/acceleratedrecoiling/mixin/ServerLevelMixin.java
@@ -6,35 +6,36 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.entity.EntityTickList;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin {
 
+    @Shadow @Final private EntityTickList entityTickList;
+
     /**
      * 重定向 ServerLevel.tick() 方法中对 entityTickList.forEach() 的调用
      */
-    @Redirect(
-            method = "tick(Ljava/util/function/BooleanSupplier;)V", // 目标方法
-            at = @At(
-                    value = "INVOKE", // 拦截类型：方法调用
-                    // 目标方法签名: void net.minecraft.world.level.entity.EntityTickList.forEach(Consumer<Entity>)
-                    target = "Lnet/minecraft/world/level/entity/EntityTickList;forEach(Ljava/util/function/Consumer;)V"
-            )
+
+    @Inject(
+            method = "tick(Ljava/util/function/BooleanSupplier;)V",
+            at = @At("RETURN")
     )
-    private void onTickEntities(EntityTickList entityTickList, Consumer<Entity> originalConsumer) {
+    private void tick(BooleanSupplier booleanSupplier, CallbackInfo ci) {
         List<LivingEntity> livingEntities = new ArrayList<>();
         List<Player> playerEntities = new ArrayList<>();
-
-        Consumer<Entity> ourConsumer = entity -> {
-            originalConsumer.accept(entity);
-
+        this.entityTickList.forEach( entity -> {
             if (!entity.isRemoved()) {
                 if (entity instanceof Player) {
                     playerEntities.add((Player) entity);
@@ -42,12 +43,30 @@ public abstract class ServerLevelMixin {
                     livingEntities.add((LivingEntity) entity);
                 }
             }
-        };
-
-        entityTickList.forEach(ourConsumer);
-
+        });
         if (ParallelAABB.useFold) {
             ParallelAABB.handleEntityPush(livingEntities);
         }
     }
+
+//    @Redirect(
+//            method = "tick(Ljava/util/function/BooleanSupplier;)V", // 目标方法
+//            at = @At(
+//                    value = "INVOKE", // 拦截类型：方法调用
+//                    // 目标方法签名: void net.minecraft.world.level.entity.EntityTickList.forEach(Consumer<Entity>)
+//                    target = "Lnet/minecraft/world/level/entity/EntityTickList;forEach(Ljava/util/function/Consumer;)V"
+//            )
+//    )
+//    private void onTickEntities(EntityTickList entityTickList, Consumer<Entity> originalConsumer) {
+//        List<LivingEntity> livingEntities = new ArrayList<>();
+//        List<Player> playerEntities = new ArrayList<>();
+//
+//        Consumer<Entity> ourConsumer = entity -> {
+//            originalConsumer.accept(entity);
+//        };
+//
+//        entityTickList.forEach(ourConsumer);
+//
+//
+//    }
 }
