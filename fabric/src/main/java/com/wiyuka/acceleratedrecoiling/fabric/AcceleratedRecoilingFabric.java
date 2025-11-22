05568a5b7b1b50a28916615a80025c1a48584a77
fireboy637 <lxx601588@outlook.com>
2025-11-22 17:50:41 +0800

    refactor with architectury

---
+++ fabric/src/main/java/com/wiyuka/acceleratedrecoiling/fabric/AcceleratedRecoilingFabric.java
@@ -0,0 +1,23 @@
+package com.wiyuka.acceleratedrecoiling.fabric;
+
+import com.wiyuka.acceleratedrecoiling.natives.NativeInterface;
+import net.fabricmc.api.ModInitializer;
+
+import com.wiyuka.acceleratedrecoiling.AcceleratedRecoiling;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
+public final class AcceleratedRecoilingFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // register commands
+        CommandRegistrationCallback.EVENT.register((dispatcher, _, _) -> {
+            AcceleratedRecoiling.init(dispatcher);
+        });
+
+        // register server stop event
+        ServerLifecycleEvents.SERVER_STOPPED.register(_ -> {
+            NativeInterface.destroy();
+        });
+    }
+}
